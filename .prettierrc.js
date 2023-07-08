9e89e8888c74fea723be08c1bbee0ed16b4a3e27
coderljw <coderljw@dingtalk.com>
2023-07-08 19:31:39 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  endOfLine: 'lf',
+  proseWrap: 'never',
+  arrowParens: 'always',
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: ['*.json5'],
+      options: {
+        singleQuote: false,
+        quoteProps: 'preserve',
+      },
+    },
+    {
+      files: ['*.yml'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
+  plugins: [
+    require.resolve('prettier-plugin-packagejson'),
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+  ],
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrder: [
+    '^(react|vue)(.*)',
+    '^@?(ant|element)(.*)',
+    '<THIRD_PARTY_MODULES>',
+    '^@/(.*)',
+    '^[.]{1,2}/(.+(?<![.]css|less|scss|sass|styl)$)',
+    '^(.*).(css|less|scss|sass|styl)$',
+  ],
+}
